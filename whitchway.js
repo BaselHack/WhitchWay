6f752d8f06591649f318f57112deec9ab1dfa631
matthi <matthi@Ubuntu1704>
2017-10-28 18:20:37 +0200

    wip

--- whitchway.js
+++ whitchway.js
@@ -1,3 +1,15 @@
+// TODO request mitschneiden
+/*
+
+- find trip von a nach b mit station id und zeit (now) als param
+- stationTable mit param station id, richtung, zeit, wie weit in die zukunft?
+
+
+
+ */
+
+
+
 function findStation(query) {
 	return [ 
 		{name : "Aeschenplatz",id :1},
@@ -7,9 +19,20 @@ function findStation(query) {
 function getArrivalTime(fromId, toId) {
 	return new Date(new Date().getTime()+(1000*60*(parseInt(fromId) + parseInt(toId))))
 }
-var url = "http://www.efa-bw.de/bvb/XML_TRIP_REQUEST2";
-$.post( url, function( data ) {
-	  console.log( data );
-	});
 
-//http://www.efa-bw.de/android_bvb/XML_TRIP_REQUEST2?type_origin=stopID&name_origin=51000010&type_destination=stopID&name_destination=53023490&execInst=verifyOnly
+
+
+
+
+var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D%27http%3A%2F%2Fwww.efa-bw.de%2Fandroid_bvb%2FXML_COORD_REQUEST%3Flanguage%3Den%26coord%3D3394151.17870016%253A893628.1039322363%253ANBWT%253A%26coordListOutputFormat%3DSTRING%26max%3D5%26inclFilter%3D1%26coordOutputFormat%3DNBWT%26type_1%3DSTOP%26radius_1%3D1000%27&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+
+$.ajax({
+    url: url,
+    type: 'get',
+    success: function (data) {
+        console.log(data);
+    }
+});
+
+
